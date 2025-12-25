372378298c42ca432464899f68f5907f047665cc
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2025-12-25 13:32:53 +1100

    Add C header for Inbox/Git Repos: jc_conformance.h
    - Path: source/firmware/JC-BIOS/include/jc_conformance.h
    - Component: Inbox/Git Repos
    - Type: C header
    - Size: 274 bytes
    - Synopsis: ifndef JC_CONFORMANCE_H

---
+++ source/firmware/JC-BIOS/include/jc_conformance.h
@@ -0,0 +1,11 @@
+#ifndef JC_CONFORMANCE_H
+#define JC_CONFORMANCE_H
+
+void jc_conformance_v0_1(void);
+void jc_conformance_v0_2(void);
+void jc_conformance_v0_3(void);
+void jc_conformance_v0_4(void);
+void jc_conformance_v0_5(void);
+void jc_conformance_v0_6(void);
+
+#endif /* JC_CONFORMANCE_H */
